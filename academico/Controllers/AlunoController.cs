8cc9944dbdefabd3ae6876eea98f971d66ee1e2e
IamArthurGama <arthurgamasoares@gmail.com>
2026-03-09 19:16:00 -0300

    werf

--- academico/Controllers/AlunoController.cs
+++ academico/Controllers/AlunoController.cs
@@ -7,6 +7,7 @@ namespace academico.Controllers
     {
         private static List<Aluno> alunos = new List<Aluno>();
         private static List<Aluno> alunos2 = new List<Aluno>();
+        private static List<Aluno> alunos3 = new List<Aluno>();
         public IActionResult Create()
         {
             return View();
