6a9117b3227ff194207f66b36efb0022fcf9d93a
MatheusMottaGit <matheusprofess@gmail.com>
2026-03-23 20:04:52 -0300

    testando interface

---
+++ academico/Repositories/IAluunoRepository.cs
@@ -0,0 +1,14 @@
+﻿using academico.Models;
+
+namespace academico.Repositories
+{
+    public interface IAluunoRepository
+    {
+        Task<IEnumerable<Aluno>> GetAll(CancellationToken cancellationToken = default);
+        Task<Aluno?> GetById(CancellationToken cancellationToken = default);
+        Task Create(CancellationToken cancellationToken = default);
+        Task Edit(CancellationToken cancellationToken = default);
+        Task Delete(CancellationToken cancellationToken = default);
+        Task<bool> Exists(CancellationToken cancellationToken = default);
+    }
+}
